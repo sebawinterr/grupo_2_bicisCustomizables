3165afac14e7345cb6e56cc422170051bfc97bd0
sebawinterr <wintersebast@gmail.com>
2020-07-04 18:07:45 -0300

    quienes somos linkeado, responsiveness para los formularios de admin

--- site/src/controllers/webController.js
+++ site/src/controllers/webController.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     index : function(req, res){
-        res.sendFile(path.resolve(__dirname, '..','views','web','index.html'));
-        
+        res.sendFile(path.resolve(__dirname, '..','views','web','index.html')); 
+    },
+    quienesSomos : function(req, res){
+        res.sendFile(path.resolve(__dirname, '..','views','web','quienesSomos.html')); 
     }
-
-    
 }
--- site/src/routes/web.js
+++ site/src/routes/web.js
@@ -5,5 +5,6 @@ const path = require('path');
 const webController = require(path.resolve(__dirname,'..','controllers','webController'));
 
 router.get('/', webController.index);
+router.get('/quienesSomos', webController.quienesSomos)
 
 module.exports = router;
