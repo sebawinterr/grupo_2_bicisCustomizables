7137910cfde116c3097ba6b985fbac0199475d8c
marianotorrecilla <marianotorrecilla89@hotmail.com>
2020-07-16 19:48:45 -0300

    partial detalle productos y su ruta

--- site/src/controllers/productosController.js
+++ site/src/controllers/productosController.js
@@ -9,12 +9,18 @@ module.exports = {
       res.render(path.resolve(__dirname, '..','views','productos','productos'),{bicicletas});
         
     },
-
-    detalle : function(req, res){
-        //res.sendFile(path.resolve(__dirname, '..','views','productos','detalle.html'));
-        res.render(path.resolve(__dirname, '..','views','productos','detalle'));
+    detalle: (req,res)=>{
+        let bicicletas = JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','bicicletas.json')));
         
-    },   
+        let miBiciDetalle;
+        bicicletas.forEach(bici => {
+           if(bici.id == req.params.id){
+               miBiciDetalle = bici;         
+            }
+        });
+        res.render(path.resolve(__dirname, '..','views','productos','detalle'), {miBiciDetalle:miBiciDetalle})
+    
+    },  
     custom : function(req, res){
         //res.sendFile(path.resolve(__dirname, '..','views','productos','customizacion.html'));
         res.render(path.resolve(__dirname, '..','views','productos','customizacion'));
--- site/src/routes/productos.js
+++ site/src/routes/productos.js
@@ -5,7 +5,7 @@ const path = require('path');
 const productosController = require(path.resolve(__dirname,'..','controllers','productosController'));
 
 router.get('/productos', productosController.index);
-router.get('/productos/detalle', productosController.detalle);
+router.get('/productos/detalle/:id', productosController.detalle);
 router.get('/productos/custom', productosController.custom);
 
 module.exports = router;
