0e3ef338d155d3f10a69209b6b5aac5b7cd0eda7
Juan Esperon <juanfesperon@gmail.com>
2020-07-25 10:55:38 -0300

    proceso register primer avance

--- site/src/controllers/userController.js
+++ site/src/controllers/userController.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const fs = require('fs');
+const { check, validationResult, body } = require('express-validator');
 
-let bicicletas =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','bicicletas.json')));
+var users =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','users.json')));
 
 module.exports = {
     index : function(req, res){
@@ -10,6 +11,35 @@ module.exports = {
     },
     login : function(req, res){
         res.render(path.resolve(__dirname, '..','views','usuarios','login'));
+    },
+    processLogIn: function(req,res){
+        let errors = validationResult(req);
+
+        if (errors.isEmpty()){
+            let usersJSON =  fs.readFileSync(path.resolve(__dirname,'..','data','users.json'));
+            let users;
+            usersJSON == "" ? users = [] : users = JSON.parse(usersJSON);
+
+            for (let i = 0; i < users.length; i++) {
+                if(users[i].email == req.body.email){
+                    if(bcrypt.compareSync(req.body.password, users[i].password)){
+                        let usuarioALoguear = users[i];
+                        break;                    
+                    }                
+                }
+            }
+
+            if(usuarioALoguear == undefined){
+                return res.render('/login', {errors: [{msg: 'Credenciales invalidas'}]});
+            }
+            req.session.usuarioALogueado = usuarioALoguear;
+            res.render('success');
+
+        }else{            
+            return res.render('login', {errors: errors.errors});
+        }
+        let usuarioALoguear;        
     }
+
     
 }
--- site/src/routes/user.js
+++ site/src/routes/user.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const multer = ('multer');
+const {check,validationResult,body} = require('express-validator');
 
+
+// Requerimos el controlador
 const userController = require(path.resolve(__dirname,'..','controllers','userController'));
+// Llamamos al archivo json
+let archivoUsers =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','users.json')));
+
+// Aca hacemos uso del storage para la imagen
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(__dirname, '../../public/images/usuarios'));    //Aquí es donde vamos a guardar la imagen
+    },
+    filename: function (req, file, cb) {
+      cb(null, 'foto' + '-' + Date.now()+ path.extname(file.originalname));      
+    }
+  })
+   
+const upload= multer({ storage })
 
 router.get('/register', userController.index);
 router.get('/login',userController.login);
+router.post('/login',[
+    check('email').isEmail().withMessage('El email no es válido'),
+    check('password').isLength({min:8}).withMessage('La contraseña debe contener al menos 8 caracteres')
+    ],userController.processLogIn);
+router.get('/check', function (req,res){
+    req.session.usuarioLogueado == undefined ? res.send('no logueaste bro') : res.redirect('/productos')
+})
 
 module.exports = router;
