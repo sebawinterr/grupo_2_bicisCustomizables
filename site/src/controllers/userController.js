6de6a14cff7b2d23841c39e131c62d2e925f873b
Juan Esperon <juanfesperon@gmail.com>
2020-07-25 12:27:11 -0300

    register

--- site/src/controllers/userController.js
+++ site/src/controllers/userController.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const fs = require('fs');
+const bcrypt = require('bcrypt');
+const multer = require('multer');
 const { check, validationResult, body } = require('express-validator');
+
 
 var users =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','users.json')));
 
@@ -9,6 +12,46 @@ module.exports = {
         //res.sendFile(path.resolve(__dirname, '..','views','usuarios','register.html'));
         res.render(path.resolve(__dirname, '..','views','usuarios','register'));
     },
+
+    processRegister: (req, res) => {
+        let errors = validationResult(req);
+        if (errors.isEmpty()) {
+          let user = {
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            dni: req.body.dni,
+            telefono: req.body.telefono,
+            direccion: req.body.direccion,
+            pisoDpto: req.body.pisoDpto ? req.body.pisoDpto : '',
+            cp: req.body.cp,
+            provincia: req.body.provincia,
+            localidad: req.body.localidad,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            imagen:  req.file ? req.file.filename : '',
+            categoria: 0 // req.body.email == '%@digitalhouse.com' ? '' : Usuario 1 = Basico, 2 = Analista, 9 = Administrador
+          }
+          let archivoUsers = fs.readFileSync(path.resolve(__dirname, '../data/users.json'), {
+            encoding: 'utf-8'
+          });
+          let users;
+          if (archivoUsers == "") {
+            users = [];
+          } else {
+            users = JSON.parse(archivoUsers);
+          };
+    
+          users.push(user);
+          usersJSON = JSON.stringify(users, null, 2);
+          fs.writeFileSync(path.resolve(__dirname, '../data/users.json'), usersJSON);
+          res.redirect('/login');
+        } else {  
+          return res.render(path.resolve(__dirname, '../views/usuarios/register'), {
+            errors: errors.mapped(),  old: req.body
+          });
+        }
+      },
+
     login : function(req, res){
         res.render(path.resolve(__dirname, '..','views','usuarios','login'));
     },
--- site/src/routes/user.js
+++ site/src/routes/user.js
@@ -12,19 +12,74 @@ const userController = require(path.resolve(__dirname,'..','controllers','userCo
 // Llamamos al archivo json
 let archivoUsers =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'..','data','users.json')));
 
-// Aca hacemos uso del storage para la imagen
+
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, '../../public/images/usuarios'));    //Aquí es donde vamos a guardar la imagen
+      cb(null, path.resolve(__dirname, '../../public/images/usuarios'));    //Aquí deben indicar donde van a guardar la imagen
     },
     filename: function (req, file, cb) {
       cb(null, 'foto' + '-' + Date.now()+ path.extname(file.originalname));      
     }
   })
    
-const upload= multer({ storage })
+  const upload= multer({ storage })
 
+
+
+
+
+
+// url de Rutas 
 router.get('/register', userController.index);
+router.post('/register', upload.single('imagen'), [
+    // VALIDACIONES
+    // NOMBRE
+    check('nombre').isLength({
+        min: 1
+      }).withMessage('El nombre es obligatorio'),
+      // APELLIDO
+    check('apellido').isLength({min: 1
+      }).withMessage('El apellido es obligatorio'),
+      // DNI
+    check('dni').isLength({min: 7, max:14
+    }).withMessage('El DNI es obligatorio'),
+    body('dni').custom( (value) =>{
+        for (let i = 0; i < archivoUsers.length; i++) {
+            if (archivoUsers[i].dni == value) {
+                return false
+            }
+        }
+        return true
+    }).withMessage('Este dni ya se encuentra registrado'),   
+        //DIRECCION
+    check('direccion').isEthereumAddress({min: 1}).withMessage('Debe colocar una dirección válida'),
+        //CP
+    check('cp').isLength({min: 4
+    }).withMessage('Debe colocar un Codigo Postal'),
+        //PROVINCIA
+    check('provincia').isLength({min: 1
+    }).withMessage('Debe colocar una provincia'),
+        //LOCALIDAD
+    check('localidad').isLength({min: 1
+    }).withMessage('La localidad es obligatoria'),
+        //EMAIL  
+    check('email').isEmail().withMessage('Agregar un email válido'),
+    // Validacion para saber si existe el email del usuario
+    body('email').custom( (value) =>{
+        for (let i = 0; i < archivoUsers.length; i++) {
+            if (archivoUsers[i].email == value) {
+                return false
+            }
+        }
+        return true
+    }).withMessage('Este email ya se encuentra registrado'),      
+    // Validacion de contraseña
+    check('password').isLength({min: 8 }).withMessage('La contraseña debe tener un mínimo de 8 caractéres')    
+],userController.processRegister);
+
+
+
 router.get('/login',userController.login);
 router.post('/login',[
     check('email').isEmail().withMessage('El email no es válido'),
